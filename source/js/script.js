a48f275449fa4e0ca7b5d9ac7317d5a3cf9c10c5
HamaHs <lusuy1995@gmail.com>
2022-11-13 16:39:53 +0200

    code is made acording to official styles, added some property to optimizing

--- source/js/script.js
+++ source/js/script.js
@@ -6,15 +6,16 @@ window.addEventListener('DOMContentLoaded', () => {
     burger.addEventListener('click', () => {
         burger.classList.toggle('burger_active');
         menu.classList.toggle('menu_active');
-        fixedWrapper.classList.toggle('wrapper_fixed')
-    })
-})
+        fixedWrapper.classList.toggle('wrapper_fixed');
+    });
+});
 
 $(document).ready(function () {
     $('ul.catalog__tabs-menu').on('click', 'li:not(.catalog__tabs-item_active)', function () {
         $(this)
             .addClass('catalog__tabs-item_active').siblings().removeClass('catalog__tabs-item_active')
-            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
+            .eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(link) {
@@ -23,9 +24,9 @@ $(document).ready(function () {
                 e.preventDefault();
                 $('.catalog__card-item').eq(i).toggleClass('catalog__card-item_active');
                 $('.catalog__card-descr').eq(i).toggleClass('catalog__card-descr_active');
-            })
+            });
         });
-    };
+    }
 
     toggleSlide('.catalog__more-link');
     toggleSlide('.catalog__back-link');
@@ -34,16 +35,19 @@ $(document).ready(function () {
     // Отримую елемент по атребуту data-modal, цей атрибут придуманий мною, і при натисканні на цю кнопку 
     // я відкриваю блок overlay і вікно callback
     $('[data-modal=callback]').on('click', function () {
-        $('.overlay, #callback').fadeIn(200);
+        $('.overlay, #callback').fadeIn(300);
+        $('body').css('overflow', 'hidden');
     });
     //При натисканні на блок modal__close я буду закривати overlay, #callback, #order і #thank
     $('.modal__close').on('click', function () {
-        $('.overlay, #callback, #order, #thank').fadeOut(200)
+        $('.overlay, #callback, #order, #thank').fadeOut(300);
+        $('body').css('overflow', 'unset');
     });
     //При натисканні на кнопку catalog__buy-button я показую .overlay і #order вікна
     $('.catalog__buy-button').on('click', function () {
-        $('.overlay, #order').fadeIn(200);
-    })
+        $('.overlay, #order').fadeIn(300);
+        $('body').css('overflow', 'hidden');
+    });
     //Для вставляння назви товара який замовляється
     $('.catalog__buy-button').each(function (i) {
         $(this).on('click', function () {
